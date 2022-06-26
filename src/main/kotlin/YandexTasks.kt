3fc784f7dfdd6b4958429f4a9815c75f27ea9c1e
Lera Kolodina <kolodina.lera322@mail.ru>
2022-06-26 05:16:45 +0300

    Solved fist task

--- src/main/kotlin/YandexTasks.kt
+++ src/main/kotlin/YandexTasks.kt
@@ -4,27 +4,31 @@ class YandexTasks {
     // from https://www.youtube.com/watch?v=30tchn0TjaM&ab_channel=alishev
 
     init {
-        getMinIntersectionOfNumb(intArrayOf(1, 2 , 2, 2,2),
-                                intArrayOf(2, 2, 1,4,5)
-                                )
+        println(getMinIntersectionOfNumb(intArrayOf(1, 2 , 2, 2, 2), intArrayOf(2, 2, 1, 4, 5)))
     }
 
-    // вывод повторяющихся чисел N раз. N = минимальное количество пересечений чисел
-    // [1, 2 , 2, 2,2] [2, 2, 1,4,5] -> [1, 2, 2] (порядок не важен)
+    // 1. вывод повторяющихся чисел N раз. N = минимальное количество пересечений чисел
+    // [1, 2 , 2, 2,2] [2, 2, 1,4, 5] -> [1, 2, 2] (порядок не важен)
     // вывод чисет столько раз, сколько они пересекаются в обоих множествах
-    fun getMinIntersectionOfNumb(a1: IntArray, a2: IntArray){
-        println("start")
-        for (el in a1){
-            var num = 0
-            if (a2.contains(el)){ // if intersects
+    fun getMinIntersectionOfNumb(a1: IntArray, a2: IntArray): MutableList<Int>{
+        var tempA1: List<Int> = a1.toMutableList()
+        var tempA2: List<Int> = a2.toMutableList()
+
+        val result: MutableList<Int> = mutableListOf()
+
+        for (el in tempA1){
+            if (tempA2.contains(el)){ // if intersects
                 // println(el) // out ALL intersect, but we need MIN count of intersects
                 // so counting numb of count in both arrays
-                num = min(a1.count { it == el }, a2.count { it == el })
-                // then out result
-                println("min=$num")
+                val num = min(tempA1.count { it == el }, tempA2.count { it == el })
 
+                // remove duplicates of this numb
+                tempA1 = tempA1.filter { it != el }
+                tempA2 = tempA2.filter { it != el }
+
+                repeat(num, { result.add(el) }) // then out result
             }
-            repeat(num, { println(el) }) // fix
         }
+        return result
     }
 }
