c27ef0624dbe4a0eff39dccb0bb2f8f3081e946b
Lera Kolodina <kolodina.lera322@mail.ru>
2022-06-27 22:09:43 +0800

    Solved all tasks from video

--- src/main/kotlin/YandexTasks.kt
+++ src/main/kotlin/YandexTasks.kt
@@ -4,12 +4,22 @@ class YandexTasks {
     // from https://www.youtube.com/watch?v=30tchn0TjaM&ab_channel=alishev
 
     init {
-        println(getMinIntersectionOfNumb(intArrayOf(1, 2 , 2, 2, 2), intArrayOf(2, 2, 1, 4, 5)))
+        val task1 = "Вывод повторяющихся чисел N раз. N = минимальное количество пересечений чисел"
+        val task2 = "Вывести букву и рядом количество ее встреч подряд"
+        val task3 = "Группировка по общим буквам"
+
+        val result1 = getMinIntersectionOfNumb(intArrayOf(1, 2 , 4, 2, 7), intArrayOf(2, 2, 1, 4, 5))
+        val result2 = getLettersAndAmountsInARow("AAAABBBCCXYZAAAAABBBBBBBBBBB")
+        val result3 = groupByCommonLetters(listOf("eat", "tea", "ate", "tan", "nat", "bat"))
+
+        println(getConditionalAndResult(task1, result1))
+        println(getConditionalAndResult(task2, result2))
+        println(getConditionalAndResult(task3, result3))
     }
 
     // 1. вывод повторяющихся чисел N раз. N = минимальное количество пересечений чисел
     // [1, 2 , 2, 2,2] [2, 2, 1,4, 5] -> [1, 2, 2] (порядок не важен)
-    // вывод чисет столько раз, сколько они пересекаются в обоих множествах
+    // вывод чисел столько раз, сколько они пересекаются в обоих множествах
     fun getMinIntersectionOfNumb(a1: IntArray, a2: IntArray): MutableList<Int>{
         var tempA1: List<Int> = a1.toMutableList()
         var tempA2: List<Int> = a2.toMutableList()
@@ -31,4 +41,47 @@ class YandexTasks {
         }
         return result
     }
+
+    // вывести букву и рядом количество ее встреч подряд (in a row)
+    // AAAABBBCCXYZAAAAABBBBBBBBBBB -> A4B3C2XYZA5B8
+    fun getLettersAndAmountsInARow(string: String): String {
+        var res = ""
+        var lastChar = string[0]
+        var numbOfRepet = 0 // the numb of char repet in string in a row
+        for (ch in string){
+            if (ch == lastChar){
+                numbOfRepet++
+            }
+            else {
+                if (numbOfRepet == 1) { // if no repets (only one char)
+                    res += "$lastChar"
+                } else {
+                    res += "$lastChar$numbOfRepet"
+                    numbOfRepet = 1 //
+                }
+            }
+            lastChar = ch
+        }
+
+        // last char handling (outputting)
+        if (numbOfRepet == 1) { // if no repets (only one char)
+            res += "$lastChar"
+        } else {
+            res += "$lastChar$numbOfRepet"
+        }
+
+        return res
+    }
+
+    // группировка по общим буквам
+    // [eat tea ate tan nat bat] -> [[ate, tea, eat], [bat], [tan, nat]]
+    fun groupByCommonLetters(sArray: List<String>): Collection<List<String>> {
+        return sArray.groupBy { it.toCharArray().sorted() }.values
+    }
+
+    // вывести условие задачи и результат
+    fun getConditionalAndResult(condition: String, task: Any): String{
+        return "Задание: $condition\n Решение: $task\n\n"
+    }
+
 }
