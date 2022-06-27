937d2db57a71919a98ec79a3f494c3c507225874
Lera Kolodina <kolodina.lera322@mail.ru>
2022-06-27 22:37:33 +0800

    Add class for beautiful task output

---
+++ src/main/kotlin/BeautifulOutput.kt
@@ -0,0 +1,8 @@
+class BeautifulOutput {
+    private var taskNumber = 0
+
+    fun getConditionalAndResult(condition: String, task: Any): String{
+        taskNumber++
+        return "\nTask $taskNumber: $condition\nSolution: $task"
+    }
+}
